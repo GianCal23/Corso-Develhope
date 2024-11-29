d6b8700f118f057dee0193f7083be1c2d6e546cb
GianCal23 <giancosimo.calabrese@gmail.com>
2024-11-29 11:41:55 +0100

    correct

---
+++ NodeJS/Postgres BD/PostgresDB.ts
@@ -0,0 +1,19 @@
+
+
+const db = pgPromise()("postgres://postgres:postgres@localhost:5432/postgres");
+
+type Planet = {
+  id: number;
+  name: string;
+};
+type Planets = Planet[];
+let planets: Planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
